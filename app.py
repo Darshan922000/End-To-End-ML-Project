3ad861fbe8eafed1348a39373a63a1388e4bd730
Darshan <darshan922000@gmail.com>
2025-01-03 15:52:15 -0500

    ready to deploy app on beanstalk...

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def predict(data: Annotated[CustomData, Form()]):
         raise CustomException(e, sys)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload= True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
     
 
 
