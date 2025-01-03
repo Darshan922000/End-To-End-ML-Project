d56f70514dcc5452a48331fd38a989a777cda314
Darshan <darshan922000@gmail.com>
2025-01-03 02:53:42 -0500

    Issue resolved..API is working perfectly

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, HTTPException
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
 from enum import Enum
 import uvicorn
 import sys
-import pickle
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
@@ -14,35 +13,7 @@ from src.logger import logging
 
 app = FastAPI()
 
-class CustomData(BaseModel):
-    features : dict
-
-@app.get("/")
-def root():
-    return 'Welcome to End-To-End ML Project....(:'
-
-@app.post("/predict/")
-def predict(data: CustomData):
-    try:
-        pred_pipeline = PredictPipeline()
-        data_df = pd.DataFrame(data.features, index=[0])
-        print(data_df)
-        logging.info("Now prediction is taking place...!")
-        prediction = pred_pipeline.pred(features = data_df)
-
-        return prediction
-    
-    except Exception as e:
-        raise CustomException(e, sys)
-
-'''if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload= True)'''
-    
-
-
-
-
-'''class Gender(str, Enum):
+class Gender(str, Enum):
     male = "male"
     female = "female"
 
@@ -84,7 +55,6 @@ class CustomData(BaseModel):
             data = {key: (value.value if isinstance(value, Enum) else value) for key, value in self.model_dump().items()}
 
             data_df = pd.DataFrame([data])
-
             data_df.rename(columns={
                 'gender': 'gender',
                 'race_ethnicity': 'race/ethnicity',
@@ -95,8 +65,92 @@ class CustomData(BaseModel):
                 'writing_score': 'writing score'
             }, inplace=True)
 
-            logging.info(f"dataframe is ready....{type(data_df)}!!!")
+            logging.info(f"dataframe is ready....{type(data_df)}, {data_df.shape}!!!")
             return data_df
         
         except Exception as e:
-            raise CustomException(e, sys)'''
+            raise HTTPException(status_code=400, detail=str(e))
+class Gender(str, Enum):
+            male = "male"
+            female = "female"
+
+class RaceEthnicity(str, Enum):
+    group_a = "group A"
+    group_b = "group B"
+    group_c = "group C"
+    group_d = "group D" 
+    group_e = "group E"
+
+class ParentalEducationLevel(str, Enum):
+    high_school = "high school"
+    associate_degree = "associate's degree"
+    bachelor_degree = "bachelor's degree"
+    master_degree = "master's degree"
+    some_college = "some college"
+    some_high_school = "some high school"
+
+class Lunch(str, Enum):
+    free_reduced = "free/reduced"
+    standard = "standard"
+
+class TestPreparation(str, Enum):
+    none = "none"
+    completed = "completed"
+
+class CustomData(BaseModel):
+    gender: Gender
+    race_ethnicity: RaceEthnicity
+    parental_level_of_education: ParentalEducationLevel
+    lunch: Lunch
+    test_preparation_course: TestPreparation
+    reading_score: Annotated[int, Field(gt=0, le=100)]  
+    writing_score: Annotated[int, Field(gt=0, le=100)]
+
+
+    def get_data_as_dataframe(self):
+        try:
+            data = {key: (value.value if isinstance(value, Enum) else value) for key, value in self.model_dump().items()}
+
+            data_df = pd.DataFrame([data])
+            data_df.rename(columns={
+                'gender': 'gender',
+                'race_ethnicity': 'race/ethnicity',
+                'parental_level_of_education': 'parental level of education',
+                'lunch': 'lunch',
+                'test_preparation_course': 'test preparation course',
+                'reading_score': 'reading score',
+                'writing_score': 'writing score'
+            }, inplace=True)
+
+            logging.info(f"dataframe is ready....{type(data_df)}, {data_df.shape}!!!")
+            return data_df
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+@app.get("/")
+def root():
+    return 'Welcome to End-To-End ML Project....(:'
+
+@app.post("/predict/")
+def predict(data: Annotated[CustomData, Form()]):
+    try:
+        pred_pipeline = PredictPipeline()
+        df = data.get_data_as_dataframe()
+        logging.info("Now prediction is taking place...!")
+
+        prediction = pred_pipeline.pred(features = df)
+
+        return f"math score prediction: {prediction}"
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload= True)
+    
+
+
+
+
--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -17,13 +17,17 @@ class PredictPipeline:
             logging.info("Model and preprocessor is loading...!!")
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path = preprocessor_path)
+
             logging.info("Model and preprocessor has loaded...!!")
-            logging.info(f"type of preprocessor is {type(preprocessor)} & type of model is {type(model)}")
-            logging.info(f"features are {features}")
+      
             scaled_data = preprocessor.transform(features)
-            logging.info(f"preprocessing is done...Scaled data is {scaled_data}, {type(scaled_data)}, {scaled_data.shape}!!")
+            
             prediction = model.predict(scaled_data)
-            return prediction
+
+            logging.info(f"Prediction = {prediction}, {type(prediction)}") # fast api support JASON format which is lightweight, human-readable, and machine-readable data interchange format.
+
+            prediction_output = int(prediction[0])
+            return prediction_output
         
         except Exception as e:
             raise CustomException(e, sys)
